6c66cc9d498a6271b067c537242125186c43ab05
Bagas Ghufron Alfaiz <bagasghufronalfaiz@gmail.com>
2022-01-31 18:00:43 +0700

    useReducer
    use reducer tutorial

---
+++ src/components/useReducer/ReducerTutorial.js
@@ -0,0 +1,43 @@
+import React, { useReducer } from "react";
+
+// const initialCount = {count: 0};
+
+function init(initialCount) {
+    return { count: initialCount };
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "increment":
+            return { count: state.count + 1 };
+        case "decrement":
+            return { count: state.count - 1 };
+        case "reset":
+            return init(action.payload);
+        default:
+            throw new Error();
+    }
+}
+
+function ReducerTutorial() {
+    const initialCount =  0;
+
+    const [state, dispatch] = useReducer(reducer, initialCount, init);
+
+    return (
+        <div>
+            Count: {state.count}
+            <button
+                onClick={() =>
+                    dispatch({ type: "reset", payload: initialCount })
+                }
+            >
+                Reset
+            </button>
+            <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+            <button onClick={() => dispatch({ type: "increment" })}>+</button>
+        </div>
+    );
+}
+
+export default ReducerTutorial;
